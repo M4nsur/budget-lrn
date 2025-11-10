462c60a4eb622f005f948e8bddc7132b8b5d754d
Mansur <vakhabov.mans@gmail.com>
2025-11-10 22:59:13 +0300

    add getSum

--- main.go
+++ main.go
@@ -4,20 +4,29 @@ import "fmt"
 
 func main() {
 	transactions := []float64{}
-	for  {
+	for {
 		transaction := inputUser()
 		if transaction == 0 {
 			break
 		} else {
 			transactions = append(transactions, transaction)
-			fmt.Println(transactions)
+			sum := getSum(transactions)
+			fmt.Printf("общий баланс:%.2f \n", sum)
 		}
 	}
 }
 
 func inputUser () (float64) {
 	var valuesUser float64
-	fmt.Println("Введите сумму, либо 0, чтобы закрыть программу")
+	fmt.Println("Введите сумму, либо N, чтобы закрыть программу")
 	fmt.Scan(&valuesUser)
 	return valuesUser
+}
+
+func getSum (transactions []float64) (float64) {
+	var sum float64
+	for _, value := range transactions {
+		sum += value
+	}
+	return sum
 }
